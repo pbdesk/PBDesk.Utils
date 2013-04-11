bfd9c6a4495ed73efe70d4474c58f538f9669f04
pinalbhatt <self@pbdesk.com>
2013-04-11 10:07:38 -0400

    PBDesk.Utils Project Initial Commit
    PBDesk.Utils Project Initial Commit - Project imported from TFS

---
+++ PBDesk.Utils/LinqExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PBDesk.Utils
+{
+    public static class LinqExtensions
+    {
+        /// <summary>
+        /// Details at http://solutionizing.net/2009/07/23/using-idisposables-with-linq/
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Kill<T>(this T obj) where T : IDisposable
+        {
+            try
+            {
+                yield return obj;
+            }
+            finally
+            {
+                if (obj != null)
+                    obj.Dispose();
+            }
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> coll, Action<T> function)
+        {
+            IEnumerator<T> en = coll.GetEnumerator();
+            while (en.MoveNext())
+                function(en.Current);
+        }
+    }
+}
